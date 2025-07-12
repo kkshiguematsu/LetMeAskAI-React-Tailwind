c539b3f3141d953fedfc6cb660fc6acbd5c5a866
kassianoshiguematsu <kassiano.e@hotmail.com>
2025-07-12 16:05:35 -0300

    add react route

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Room } from './pages/room'
+import { CreateRoom } from './pages/create-room'
+
 export const App = () => {
   return (
-    <div>helloworld</div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index/>
+        <Route element={<Room />} path='/room'/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
