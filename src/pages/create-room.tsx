18c535f376bb8721e5ba20c0a4992d37816e25c9
kassianoshiguematsu <kassiano.e@hotmail.com>
2025-07-13 12:44:22 -0300

    add cards

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,32 +1,79 @@
+import { Badge } from "@/components/ui/badge"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { dayjs } from "@/lib/dayjs"
 import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router"
 
 type GetRoomsAPIResponse = Array<{
-  id: string,
-  name: string,
+  id: string
+  name: string
+  questionCount: number
+  createdAt: string
 }>
 
 export const CreateRoom = () => {
   const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
+    queryKey: ["get-rooms"],
     queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
+      const response = await fetch("http://localhost:3333/rooms")
       const result: GetRoomsAPIResponse = await response.json()
 
       return result
     },
   })
 
-  console.log(data)
-  
   return (
-    <div>
-      <div>CreateRoom Page</div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div></div>
 
-      {isLoading && <p>Carregando...</p>}
-      {data?.map((room) => {
-        return <Link to={`/room/${room.id}`} key={room.id}>{room.name}</Link>
-      })}
+          <Card>
+            <CardHeader>
+              <CardTitle>Salas recentes</CardTitle>
+              <CardDescription>
+                Acesso rápido para as salas criadas recentemente
+              </CardDescription>
+              <CardContent className="flex flex-col gap-3">
+                {isLoading && (
+                  <p className="text-muted-foreground text-sm">
+                    Carregando salas...
+                  </p>
+                )}
+
+                {data?.map((room) => {
+                  return (
+                    <Link
+                      to={`/rooms/${room.id}`}
+                      key={room.id}
+                      className="hover:bg-accent/50 flex items-center justify-between rounded-lg border p-3"
+                    >
+                      <div className="flex flex-1 flex-col gap-1">
+                        <h3 className="font-medium">{room.name}</h3>
+
+                        <div className="flex items-center gap-2">
+                          <Badge className="text-xs" variant="secondary">
+                            {dayjs(room.createdAt).toNow()}
+                          </Badge>
+                          <Badge className="text-xs" variant="secondary">
+                            {room.questionCount} perguntas(s)
+                          </Badge>
+                        </div>
+                      </div>
+                    </Link>
+                  )
+                })}
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 }
