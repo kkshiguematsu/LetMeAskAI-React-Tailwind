512e599a17e2a5e0fa251af905c2b25c132f91d3
kassianoshiguematsu <kassiano.e@hotmail.com>
2025-07-13 18:59:57 -0300

    feat: add start and stop recording

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Room } from './pages/room'
-import { CreateRoom } from './pages/create-room'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Room } from "./pages/room"
+import { CreateRoom } from "./pages/create-room"
+import { RecordRoomAudio } from "./pages/record-room-audio"
 
 const queryClient = new QueryClient()
 
@@ -10,11 +11,11 @@ export const App = () => {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route element={<CreateRoom />} index/>
-          <Route element={<Room />} path='/room/:roomId'/>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   )
 }
-
